0805884fbe872d4826810e055fa8e23cf6839f4b
Tofic <mehrazitawfik@gmail.com>
2021-07-13 17:01:13 -0400

    add the screenshot for MyTheme

---
+++ wp-content/themes/MyTheme/index.php
@@ -0,0 +1,15 @@
+<?php
+
+if ( have_posts() ) {
+    while ( have_posts() ) {
+ 
+        the_post(); ?>
+ 
+        <h2><?php the_title(); ?></h2>
+ 
+        <?php the_content(); 
+ 
+     }
+}else { echo 'No content found';}
+
+?>
