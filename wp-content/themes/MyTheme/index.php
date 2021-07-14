c4b7ce8bab625f5c0858c8f0eab69c39e4c93b95
Tofic <mehrazitawfik@gmail.com>
2021-07-14 01:17:09 -0400

    Get works have_posts by initializing WP_Query()

--- wp-content/themes/MyTheme/index.php
+++ wp-content/themes/MyTheme/index.php
@@ -1,15 +1,22 @@
-<?php
+<?php 
 
-if ( have_posts() ) {
-    while ( have_posts() ) {
- 
-        the_post(); ?>
- 
-        <h2><?php the_title(); ?></h2>
- 
-        <?php the_content(); 
- 
-     }
-}else { echo 'No content found';}
+  $wp_query = new WP_Query('posts_per_page=5&orderby=title');
+
+
+      if ( $wp_query->have_posts()) :
+      
+        while ($wp_query->have_posts()) : $wp_query->the_post();?>
+
+        <h3><?php the_title();?></h3>
+        <p><?php the_content(); ?></p>  
+
+        <?php  wp_reset_postdata();
+       endwhile;
+
+  else :
+    echo 'No Content found!';
+  endif;
 
 ?>
+
+
