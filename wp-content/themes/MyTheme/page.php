d45ce9f42d386975836e891f391bc4d622fe1a54
Tofic <mehrazitawfik@gmail.com>
2021-07-18 15:14:31 -0400

    header margin 5px and removing the space below children links

--- wp-content/themes/MyTheme/page.php
+++ wp-content/themes/MyTheme/page.php
@@ -7,7 +7,7 @@
         <article class="post page">
           <?php 
           
-          if(has_children() OR $post->post_parent > 0 ){  ?>
+          if(has_children() OR $post->post_parent > 0 ){   ?>
 
         <nav class="site-nav children-links clearfix">
                     <span class="parent-link"><a href="<?php get_permalink(get_top_ancestor_id()); ?>"><?php echo get_the_title(get_top_ancestor_id()); ?></a></span>
@@ -20,7 +20,7 @@
                               wp_list_pages($args);   
                               ?>
                     </ul>
-         </nav> &nbsp; <?php  } ?>
+         </nav> <?php  } ?>
 
           <h2><?php the_title();?> | I am the page.php file</h2>
           <p><?php the_content(); ?></p> 
