fb9e6fa9f1537504287f331610c85fa48697fab4
Tofic <mehrazitawfik@gmail.com>
2021-07-24 14:48:54 -0400

    add columns

--- wp-content/themes/MyTheme/index.php
+++ wp-content/themes/MyTheme/index.php
@@ -1,23 +1,74 @@
 <?php 
   get_header();
 
-  $wp_query = new WP_Query('posts_per_page=5&orderby=title');
+  $wp_query = new WP_Query('posts_per_page=5');
 
-                      if ( $wp_query->have_posts()) :
-                        while ($wp_query->have_posts()) : $wp_query->the_post();?>
+               ?>   <div class="flex-container">
+                        <div class="left-column">
 
-                              <article class="post">
-                                <h2><a href="<?php echo get_permalink(); ?>"><?php the_title();?></a></h2>
-                                <p><?php the_content(); ?></p> 
-                              </article>
 
-                        <?php  wp_reset_postdata();
-                      endwhile;
+                              <?php    if ( $wp_query->have_posts()) :
+                                  while ($wp_query->have_posts()) : $wp_query->the_post();?>
 
-                      else :
-                        echo 'No Content found!';
-                      endif; 
-  get_footer();
+                                        <article class="post">
+                                          <h2><a href="<?php echo get_permalink(); ?>"><?php the_title();?></a></h2>
+                                          <i><?php the_date(); ?></i>
+                                          <p><?php the_excerpt(); ?></p> 
+                                        </article>
+
+                                  <?php  wp_reset_postdata();
+                                endwhile;
+
+                                else :
+                                  echo 'No Content found!';
+                                endif; ?>
+
+                        </div>
+
+                        <div class="middle-column">
+
+                               <?php    if ( $wp_query->have_posts()) :
+                                  while ($wp_query->have_posts()) : $wp_query->the_post();?>
+
+                                        <article class="post">
+                                          <h2><a href="<?php echo get_permalink(); ?>"><?php the_title();?></a></h2>
+                                          <i><?php the_date(); ?></i>
+                                          <p><?php the_excerpt(); ?></p> 
+                                        </article>
+
+                                  <?php  wp_reset_postdata();
+                                endwhile;
+
+                                else :
+                                  echo 'No Content found!';
+                                endif; ?>
+
+
+                        </div>
+
+                        <div class="right-column">
+                              <h2>Last posts:</h2><br>
+                              <?php    if ( $wp_query->have_posts()) :
+                                  while ($wp_query->have_posts()) : $wp_query->the_post();?>
+
+                                        <article class="post">
+                                          <h3><a href="<?php echo get_permalink(); ?>"><?php the_title();?></a></h3>
+                                          <i><?php the_date(); ?></i>
+                            
+                                        </article>
+
+                                  <?php  wp_reset_postdata();
+                                endwhile;
+
+                                else :
+                                  echo 'No Content found!';
+                                endif; ?>
+
+                        </div>
+
+                      </div>
+
+ <?php get_footer();
 ?>
 
 
